78def9737815a86e9bbd9ed8996a742e9577bc84
Isadorass <isadorass1710@gmail.com>
2021-07-30 20:30:28 -0300

    Isa linda, gostosa, te amo

---
+++ WinFormsPresentationLayer/FormCadastroDeClientes.Designer.cs
@@ -0,0 +1,354 @@
+﻿
+namespace WinFormsPresentationLayer
+{
+    partial class FormCadastroDeClientes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtpDataMatricula = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblTelefoneFixo = new System.Windows.Forms.Label();
+            this.txtTelefoneFixo = new System.Windows.Forms.TextBox();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.cmbGenero = new System.Windows.Forms.ComboBox();
+            this.txtTelefoneCelular = new System.Windows.Forms.TextBox();
+            this.lblDataNascimento = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.dtpDataNascimento = new System.Windows.Forms.DateTimePicker();
+            this.lblCPF = new System.Windows.Forms.Label();
+            this.lblGenero = new System.Windows.Forms.Label();
+            this.txtCPF = new System.Windows.Forms.TextBox();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnAtualizar = new System.Windows.Forms.Button();
+            this.btnCadastrar = new System.Windows.Forms.Button();
+            this.dgvCadastroClientes = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtRG = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCadastroClientes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dtpDataMatricula
+            // 
+            this.dtpDataMatricula.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpDataMatricula.Location = new System.Drawing.Point(163, 164);
+            this.dtpDataMatricula.Name = "dtpDataMatricula";
+            this.dtpDataMatricula.Size = new System.Drawing.Size(119, 20);
+            this.dtpDataMatricula.TabIndex = 48;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.ForeColor = System.Drawing.SystemColors.Window;
+            this.label2.Location = new System.Drawing.Point(160, 148);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 13);
+            this.label2.TabIndex = 49;
+            this.label2.Text = "Data da Matrícula";
+            // 
+            // lblTelefoneFixo
+            // 
+            this.lblTelefoneFixo.AutoSize = true;
+            this.lblTelefoneFixo.BackColor = System.Drawing.Color.Transparent;
+            this.lblTelefoneFixo.ForeColor = System.Drawing.SystemColors.Window;
+            this.lblTelefoneFixo.Location = new System.Drawing.Point(230, 198);
+            this.lblTelefoneFixo.Name = "lblTelefoneFixo";
+            this.lblTelefoneFixo.Size = new System.Drawing.Size(71, 13);
+            this.lblTelefoneFixo.TabIndex = 43;
+            this.lblTelefoneFixo.Text = "Telefone Fixo";
+            // 
+            // txtTelefoneFixo
+            // 
+            this.txtTelefoneFixo.Location = new System.Drawing.Point(233, 214);
+            this.txtTelefoneFixo.Name = "txtTelefoneFixo";
+            this.txtTelefoneFixo.Size = new System.Drawing.Size(213, 20);
+            this.txtTelefoneFixo.TabIndex = 42;
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.BackColor = System.Drawing.Color.Transparent;
+            this.lblNome.ForeColor = System.Drawing.SystemColors.Window;
+            this.lblNome.Location = new System.Drawing.Point(23, 41);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(35, 13);
+            this.lblNome.TabIndex = 39;
+            this.lblNome.Text = "Nome";
+            // 
+            // cmbGenero
+            // 
+            this.cmbGenero.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbGenero.FormattingEnabled = true;
+            this.cmbGenero.Items.AddRange(new object[] {
+            "Masculino",
+            "Feminino",
+            "Indeterminado(a)"});
+            this.cmbGenero.Location = new System.Drawing.Point(292, 162);
+            this.cmbGenero.Name = "cmbGenero";
+            this.cmbGenero.Size = new System.Drawing.Size(154, 21);
+            this.cmbGenero.TabIndex = 38;
+            // 
+            // txtTelefoneCelular
+            // 
+            this.txtTelefoneCelular.Location = new System.Drawing.Point(25, 214);
+            this.txtTelefoneCelular.Name = "txtTelefoneCelular";
+            this.txtTelefoneCelular.Size = new System.Drawing.Size(202, 20);
+            this.txtTelefoneCelular.TabIndex = 46;
+            // 
+            // lblDataNascimento
+            // 
+            this.lblDataNascimento.AutoSize = true;
+            this.lblDataNascimento.BackColor = System.Drawing.Color.Transparent;
+            this.lblDataNascimento.ForeColor = System.Drawing.SystemColors.Window;
+            this.lblDataNascimento.Location = new System.Drawing.Point(22, 146);
+            this.lblDataNascimento.Name = "lblDataNascimento";
+            this.lblDataNascimento.Size = new System.Drawing.Size(104, 13);
+            this.lblDataNascimento.TabIndex = 44;
+            this.lblDataNascimento.Text = "Data de Nascimento";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.SystemColors.Window;
+            this.label1.Location = new System.Drawing.Point(22, 198);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 47;
+            this.label1.Text = "Telefone Celular";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.BackColor = System.Drawing.Color.Transparent;
+            this.lblEmail.ForeColor = System.Drawing.SystemColors.Window;
+            this.lblEmail.Location = new System.Drawing.Point(23, 95);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(35, 13);
+            this.lblEmail.TabIndex = 41;
+            this.lblEmail.Text = "E-mail";
+            // 
+            // txtNome
+            // 
+            this.txtNome.BackColor = System.Drawing.SystemColors.Window;
+            this.txtNome.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtNome.Location = new System.Drawing.Point(26, 57);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(256, 20);
+            this.txtNome.TabIndex = 34;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(25, 112);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(257, 20);
+            this.txtEmail.TabIndex = 36;
+            // 
+            // dtpDataNascimento
+            // 
+            this.dtpDataNascimento.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpDataNascimento.Location = new System.Drawing.Point(25, 164);
+            this.dtpDataNascimento.Name = "dtpDataNascimento";
+            this.dtpDataNascimento.Size = new System.Drawing.Size(132, 20);
+            this.dtpDataNascimento.TabIndex = 37;
+            // 
+            // lblCPF
+            // 
+            this.lblCPF.AutoSize = true;
+            this.lblCPF.BackColor = System.Drawing.Color.Transparent;
+            this.lblCPF.ForeColor = System.Drawing.SystemColors.Window;
+            this.lblCPF.Location = new System.Drawing.Point(289, 41);
+            this.lblCPF.Name = "lblCPF";
+            this.lblCPF.Size = new System.Drawing.Size(27, 13);
+            this.lblCPF.TabIndex = 40;
+            this.lblCPF.Text = "CPF";
+            // 
+            // lblGenero
+            // 
+            this.lblGenero.AutoSize = true;
+            this.lblGenero.BackColor = System.Drawing.Color.Transparent;
+            this.lblGenero.ForeColor = System.Drawing.SystemColors.Window;
+            this.lblGenero.Location = new System.Drawing.Point(289, 146);
+            this.lblGenero.Name = "lblGenero";
+            this.lblGenero.Size = new System.Drawing.Size(42, 13);
+            this.lblGenero.TabIndex = 45;
+            this.lblGenero.Text = "Gênero";
+            // 
+            // txtCPF
+            // 
+            this.txtCPF.BackColor = System.Drawing.SystemColors.Window;
+            this.txtCPF.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtCPF.Location = new System.Drawing.Point(292, 57);
+            this.txtCPF.Name = "txtCPF";
+            this.txtCPF.Size = new System.Drawing.Size(154, 20);
+            this.txtCPF.TabIndex = 35;
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.BackColor = System.Drawing.Color.LightSlateGray;
+            this.btnEditar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEditar.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEditar.Location = new System.Drawing.Point(343, 260);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(103, 34);
+            this.btnEditar.TabIndex = 53;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = false;
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.BackColor = System.Drawing.Color.LightSlateGray;
+            this.btnExcluir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExcluir.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExcluir.Location = new System.Drawing.Point(244, 260);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(93, 34);
+            this.btnExcluir.TabIndex = 52;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = false;
+            // 
+            // btnAtualizar
+            // 
+            this.btnAtualizar.BackColor = System.Drawing.Color.LightSlateGray;
+            this.btnAtualizar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAtualizar.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAtualizar.Location = new System.Drawing.Point(136, 260);
+            this.btnAtualizar.Name = "btnAtualizar";
+            this.btnAtualizar.Size = new System.Drawing.Size(102, 34);
+            this.btnAtualizar.TabIndex = 51;
+            this.btnAtualizar.Text = "Atualizar";
+            this.btnAtualizar.UseVisualStyleBackColor = false;
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.BackColor = System.Drawing.Color.LightSlateGray;
+            this.btnCadastrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCadastrar.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCadastrar.Location = new System.Drawing.Point(25, 260);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(105, 34);
+            this.btnCadastrar.TabIndex = 50;
+            this.btnCadastrar.Text = "Cadastrar";
+            this.btnCadastrar.UseVisualStyleBackColor = false;
+            // 
+            // dgvCadastroClientes
+            // 
+            this.dgvCadastroClientes.BackgroundColor = System.Drawing.Color.LightSlateGray;
+            this.dgvCadastroClientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCadastroClientes.Location = new System.Drawing.Point(467, 57);
+            this.dgvCadastroClientes.Name = "dgvCadastroClientes";
+            this.dgvCadastroClientes.Size = new System.Drawing.Size(254, 237);
+            this.dgvCadastroClientes.TabIndex = 59;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.ForeColor = System.Drawing.SystemColors.Window;
+            this.label3.Location = new System.Drawing.Point(289, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 13);
+            this.label3.TabIndex = 61;
+            this.label3.Text = "RG";
+            // 
+            // txtRG
+            // 
+            this.txtRG.BackColor = System.Drawing.SystemColors.Window;
+            this.txtRG.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtRG.Location = new System.Drawing.Point(292, 113);
+            this.txtRG.Name = "txtRG";
+            this.txtRG.Size = new System.Drawing.Size(154, 20);
+            this.txtRG.TabIndex = 60;
+            // 
+            // FormCadastroDeClientes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WinFormsPresentationLayer.Properties.Resources.fundo_de_produto_azul_escuro_53876_895031;
+            this.ClientSize = new System.Drawing.Size(744, 343);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtRG);
+            this.Controls.Add(this.dgvCadastroClientes);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnAtualizar);
+            this.Controls.Add(this.btnCadastrar);
+            this.Controls.Add(this.dtpDataMatricula);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblTelefoneFixo);
+            this.Controls.Add(this.txtTelefoneFixo);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.cmbGenero);
+            this.Controls.Add(this.txtTelefoneCelular);
+            this.Controls.Add(this.lblDataNascimento);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.dtpDataNascimento);
+            this.Controls.Add(this.lblCPF);
+            this.Controls.Add(this.lblGenero);
+            this.Controls.Add(this.txtCPF);
+            this.Name = "FormCadastroDeClientes";
+            this.Text = "Cadastrar Clientes";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCadastroClientes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dtpDataMatricula;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblTelefoneFixo;
+        private System.Windows.Forms.TextBox txtTelefoneFixo;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.ComboBox cmbGenero;
+        private System.Windows.Forms.TextBox txtTelefoneCelular;
+        private System.Windows.Forms.Label lblDataNascimento;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.DateTimePicker dtpDataNascimento;
+        private System.Windows.Forms.Label lblCPF;
+        private System.Windows.Forms.Label lblGenero;
+        private System.Windows.Forms.TextBox txtCPF;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnAtualizar;
+        private System.Windows.Forms.Button btnCadastrar;
+        private System.Windows.Forms.DataGridView dgvCadastroClientes;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtRG;
+    }
+}
